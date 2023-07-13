53c1a674542a498f9cfcc3b744d2c211c0dfec5a
risenfromashes <ashrafurkhan37@gmail.com>
2023-07-13 19:39:59 +0600

    update server

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 // Constants
 const PORT = 3001;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 // App
 const app = express();
 
 function decodeBase64(str) {
-	return Buffer.from(str, 'base64').toString('ascii');
+  return Buffer.from(str, "base64").toString("ascii");
 }
 
-app.get('/', (req, res) => {
-	res.send(decodeBase64(process.env.DB_USERNAME));
+app.get("/", (req, res) => {
+  res.send("not hello");
 });
 
 app.listen(PORT, HOST);
