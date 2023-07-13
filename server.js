e3e68c02aa518b7e94222a03c23e1b1902975bc9
risenfromashes <ashrafurkhan37@gmail.com>
2023-07-14 03:28:53 +0600

    update kube

--- server.js
+++ server.js
@@ -2,6 +2,17 @@
 const express = require("express");
 const app = express();
 const pool = require("./pool");
+const prom = require("prom-client");
+
+const collectDefaultMetrics = prom.collectDefaultMetrics();
+const registry = new prom.Registry();
+const httpRequestTimer = new prom.Histogram({
+  name: "http_request_duration_seconds",
+  help: "Duration of HTTP requests in seconds",
+  labelNames: ["method", "route", "code"],
+  buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10], // 0.1 to 10 seconds
+});
+registry.registerMetric(httpRequestTimer);
 
 const cors = require("cors");
 const port = 3001;
@@ -11,14 +22,24 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("frontend/dist"));
 
+app.get("/metrics", async (req, res) => {
+  try {
+    res.set("Content-Type", prom.register.contentType);
+    res.end(await prom.register.metrics());
+  } catch (ex) {
+    res.status(500).end(ex);
+  }
+});
+
 // Define a route to handle GET requests
 app.get("/api/hello", (req, res) => {
   res.send("Hello, World!");
 });
 
 app.post("/login", (req, response) => {
+  const end = httpRequestTimer.startTimer();
+  const route = req.route.path;
   const formData = req.body;
-  console.log(formData);
   try {
     const query = `
       SELECT * FROM "User" WHERE nid = '${formData.nid}';
@@ -27,26 +48,30 @@ app.post("/login", (req, response) => {
       if (err) {
         console.log(err);
         response.sendStatus(400);
+        end({ route, code: response.statusCode, method: req.method });
         return;
       }
       console.log("Received response from database.");
       if (res.rows.length == 0) {
         response.sendStatus(400);
+        end({ route, code: response.statusCode, method: req.method });
         return;
       } else {
         console.log("User found!");
         response.status(200).json(res.rows[0].name);
-        for (let row of res.rows) {
-          console.log(row);
-        }
+        end({ route, code: response.statusCode, method: req.method });
       }
     });
   } catch (e) {
     console.log(e);
+    response.status(500).end(e);
+    end({ route, code: response.statusCode, method: req.method });
   }
 });
 
 app.post("/register", (req, response) => {
+  const end = httpRequestTimer.startTimer();
+  const route = req.route.path;
   const formData = req.body;
   console.log(formData);
   try {
@@ -58,11 +83,13 @@ app.post("/register", (req, response) => {
       if (err) {
         console.log(err);
         response.sendStatus(400);
+        end({ route, code: response.statusCode, method: req.method });
         return;
       }
       if (res.rows.length > 0) {
         console.log("User already exists!");
         response.sendStatus(400);
+        end({ route, code: response.statusCode, method: req.method });
         return;
       }
       const query2 = `
@@ -72,18 +99,24 @@ app.post("/register", (req, response) => {
         if (err) {
           console.log(err);
           response.sendStatus(400);
+          end({ route, code: response.statusCode, method: req.method });
         } else {
           response.sendStatus(200);
           console.log("User added to database!");
+          end({ route, code: response.statusCode, method: req.method });
         }
       });
     });
   } catch (e) {
     console.log(e);
+    response.status(500).end(e);
+    end({ route, code: response.statusCode, method: req.method });
   }
 });
 
 app.post("/getdate", (req, response) => {
+  const end = httpRequestTimer.startTimer();
+  const route = req.route.path;
   const formData = req.body;
   console.log(formData);
   try {
@@ -94,6 +127,7 @@ app.post("/getdate", (req, response) => {
       if (err) {
         console.log(err);
         response.sendStatus(400);
+        end({ route, code: response.statusCode, method: req.method });
         return;
       }
       console.log("Received response from database.");
@@ -101,6 +135,7 @@ app.post("/getdate", (req, response) => {
         // date not yet allocated
         response.sendStatus(400);
         response.status(200).json({ date: null, status: false });
+        end({ route, code: response.statusCode, method: req.method });
         return;
       } else {
         console.log("User found!");
@@ -108,6 +143,7 @@ app.post("/getdate", (req, response) => {
           date: res.rows[0].vaccination_date,
           status: res.rows[0].is_vaccinated,
         });
+        end({ route, code: response.statusCode, method: req.method });
         for (let row of res.rows) {
           console.log(row);
         }
@@ -115,10 +151,14 @@ app.post("/getdate", (req, response) => {
     });
   } catch (e) {
     console.log(e);
+    response.status(500).end(e);
+    end({ route, code: response.statusCode, method: req.method });
   }
 });
 
 app.post("/allocatedate", (req, response) => {
+  const end = httpRequestTimer.startTimer();
+  const route = req.route.path;
   const formData = req.body;
   console.log(formData);
   try {
@@ -132,6 +172,7 @@ app.post("/allocatedate", (req, response) => {
       if (err) {
         console.log(err);
         response.sendStatus(400);
+        end({ route, code: response.statusCode, method: req.method });
         return;
       }
       console.log("Received response from database.");
@@ -139,13 +180,14 @@ app.post("/allocatedate", (req, response) => {
         // date not yet allocated
         response.sendStatus(400);
         response.status(200).json({ date: null, status: false });
-        return;
+        end({ route, code: response.statusCode, method: req.method });
       } else {
         console.log("User found!");
         response.status(200).json({
           date: res.rows[0].vaccination_date,
           status: res.rows[0].is_vaccinated,
         });
+        end({ route, code: response.statusCode, method: req.method });
         for (let row of res.rows) {
           console.log(row);
         }
@@ -153,6 +195,8 @@ app.post("/allocatedate", (req, response) => {
     });
   } catch (e) {
     console.log(e);
+    response.status(500).end();
+    end({ route, code: response.statusCode, method: req.method });
   }
 });
 
