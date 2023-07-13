d568ea63a65960092f6699d543557083f38b0009
risenfromashes <ashrafurkhan37@gmail.com>
2023-07-13 18:49:49 +0600

    add secret

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 
+function decodeBase64(str) {
+	return Buffer.from(str, 'base64').toString('ascii');
+}
+
 app.get('/', (req, res) => {
-  res.send(process.env.DB_USERNAME);
+	res.send(decodeBase64(process.env.DB_USERNAME));
 });
 
 app.listen(PORT, HOST);
